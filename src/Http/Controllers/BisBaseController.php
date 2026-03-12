62922d93c41a2b3d579f6f88af78313cc0655e4e
jadergabriel <programacaoeti@outlook.com>
2026-03-12 11:24:57 -0300

    Ajuste de Controller

--- src/Http/Controllers/BisBaseController.php
+++ src/Http/Controllers/BisBaseController.php
@@ -4,11 +4,21 @@ namespace iEducar\Packages\Bis\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use iEducar\Packages\Bis\BisProcess;
+use Illuminate\Support\Facades\Auth;
 
 abstract class BisBaseController extends Controller
 {
     public function __construct()
     {
+        // Em contexto de console (ex.: php artisan route:list) não há usuário autenticado.
+        if (app()->runningInConsole()) {
+            return;
+        }
+
+        if (! Auth::check()) {
+            return;
+        }
+
         $this->menu(BisProcess::menuBi());
     }
 }
